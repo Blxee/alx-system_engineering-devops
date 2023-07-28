815865365184de353d898df941784a6584e1fe6d
Blxee <aminedev57@gmail.com>
2023-07-28 16:56:56 +0100

    Fixed: 102-zombie.c task

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,22 +2,41 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/**
+ * infinite_while - runs an inginite loop
+ *
+ * Return: always 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	int i;
-	pid_t pid = 1;
+	pid_t pid;
 
-	for (i = 0; i <= 5; i++)
+	for (i = 0; i < 5; i++)
 	{
+		pid = fork();
 		if (pid > 0)
-			pid = fork();
-		else if (pid == 0)
 		{
-			printf("Zombie process created, PID: %d\n", getpid());
-			while (1)
-				;
+			printf("Zombie process created, PID: %d\n", pid);
 		}
+		else if (pid == 0 || pid == -1)
+			break;
 	}
 
+	infinite_while();
 	return (0);
 }
