cbff60b5b60e1f78dbbafc41531083df1c68889c
Blxee <aminedev57@gmail.com>
2023-09-23 12:20:24 +0100

    Added: 100-puppet_ssh_config.pp task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,11 @@
 # 4. Client configuration file (w/ Puppet)
 
-file { '/etc/ssh/ssh_config':
-  content => 'Host alx-server\n\tHostName 100.25.152.12\n\tUser ubuntu\n\tIdentifyFile ~/.ssh/school\n\tPasswordAuthentication no',
+file_line { '/etc/ssh/ssh_config':
+  line  => 'IdentifyFile ~/.ssh/school',
+  match => 'IdentifyFile',
+}
+
+file_line { '/etc/ssh/ssh_config':
+  line  => 'PasswordAuthentication no',
+  match => 'PasswordAuthentication',
 }
