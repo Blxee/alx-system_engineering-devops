4590b4712ccd1dc1360bcc165167c72666e2e016
Blxee <aminedev57@gmail.com>
2023-09-23 12:23:01 +0100

    Added: 100-puppet_ssh_config.pp task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # 4. Client configuration file (w/ Puppet)
 
-file_line { '/etc/ssh/ssh_config':
+file_line { 'set key to use':
+  path  => '/etc/ssh/ssh_config',
   line  => 'IdentifyFile ~/.ssh/school',
   match => 'IdentifyFile',
 }
 
-file_line { '/etc/ssh/ssh_config':
+file_line { 'turn off password':
+  path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => 'PasswordAuthentication',
 }
