e72211c536135fee7deb874e9da415c3d107d84a
Blxee <aminedev57@gmail.com>
2023-09-26 23:12:00 +0100

    Added: 7-puppet_install_nginx_web_server.pp task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,18 @@
 # 5. Install Nginx web server (w/ Puppet)
-class { 'nginx':
-  manage_repo => true,
-  service_enable => true,
-  service_ensure => 'running',
+package { 'nginx':
+  ensure => 'installed',
 }
 
-nginx::resource::vhost { 'default':
-  www_root    => '/var/www/html',
-  listen_ip   => '0.0.0.0',
-  listen_port => '80',
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
 }
 
-nginx::resource::location { 'default':
-  location    => '/',
-  vhost       => 'default',
-  ensure      => present,
-  content     => 'Hello World!',
+file { '/etc/nginx/sites-available/default':
+  notify => Service['nginx'],
 }
 
-nginx::resource::location { 'redirect':
-  location       => '/redirect_me',
-  vhost          => 'default',
-  ensure         => present,
-  rewrite_rules  => ['^/redirect_me$ https://example.com permanent;'],
+file { '/var/www/html/index.html':
+  ensure => 'present',
+  content => 'Hello World!',
 }
