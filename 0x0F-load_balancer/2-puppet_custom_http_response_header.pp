53bfc554584efe922f6e49180a37e8d895ab2a33
Blxee <aminedev57@gmail.com>
2023-10-03 01:10:33 +0100

    Fixed: 2-puppet_custom_http_response_header.pp task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# 2. Add a custom HTTP header with Puppet
+
+exec { 'update':
+  command => 'sudo apt-get -y update',
+}
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update'],
+}
+
+file_line { 'add header':
+  path    => '/etc/nginx/nginx.conf',
+  line    => 'add_header X-Served-By $hostname;',
+  after   => 'server_name .*;',
+  match   => '^.*server_name .*;$',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
