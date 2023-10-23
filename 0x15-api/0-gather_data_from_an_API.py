29e529331c8fe7ce537b9cdb908055f8021b3631
Blxee <aminedev57@gmail.com>
2023-10-23 14:42:05 +0100

    Added: task 0 of 0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,18 +4,19 @@ import json
 from urllib import request
 from sys import argv
 
-emp_id = argv[1]
-url = 'https://jsonplaceholder.typicode.com/users/' + emp_id
+if __name__ == '__main__':
+    emp_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/' + emp_id
 
-with request.urlopen(url) as response:
-    emp_name = json.load(response).get('name')
+    with request.urlopen(url) as response:
+        emp_name = json.load(response).get('name')
 
-with request.urlopen(url + '/todos/') as response:
-    todos = json.load(response)
+    with request.urlopen(url + '/todos/') as response:
+        todos = json.load(response)
 
-tasks_done = len(list(filter(lambda t: t.get('completed'), todos)))
+    done = len(list(filter(lambda t: t.get('completed'), todos)))
 
-print(f'Employee {emp_name} is done with tasks({tasks_done}/{len(todos)}):')
-for todo in todos:
-    if todo.get('completed'):
-        print('\t', todo.get('title'))
+    print(f'Employee {emp_name} is done with tasks({done}/{len(todos)}):')
+    for todo in todos:
+        if todo.get('completed'):
+            print('\t', todo.get('title'))
