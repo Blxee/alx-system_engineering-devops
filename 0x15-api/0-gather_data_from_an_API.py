983991eaf7fa70152baf5e37c9b72648cafad53d
Blxee <aminedev57@gmail.com>
2023-10-23 14:39:36 +0100

    Added: task 0 of 0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,14 +8,14 @@ emp_id = argv[1]
 url = 'https://jsonplaceholder.typicode.com/users/' + emp_id
 
 with request.urlopen(url) as response:
-    emp_name = json.load(response)['name']
+    emp_name = json.load(response).get('name')
 
 with request.urlopen(url + '/todos/') as response:
     todos = json.load(response)
 
-tasks_done = len(list(filter(lambda t: t['completed'], todos)))
+tasks_done = len(list(filter(lambda t: t.get('completed'), todos)))
 
 print(f'Employee {emp_name} is done with tasks({tasks_done}/{len(todos)}):')
 for todo in todos:
-    if todo['completed']:
-        print('\t', todo['title'])
+    if todo.get('completed'):
+        print('\t', todo.get('title'))
