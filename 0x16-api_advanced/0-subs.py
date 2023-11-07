0cd52f8630d89283eb3c99bad19c877eb24f84f7
Blxee <aminedev57@gmail.com>
2023-11-07 20:32:40 +0100

    Fixed: task 0 of 0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,8 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribers a subreddit has or 0 """
     response = requests.get(
-        'https://www.reddit.com/r/{subreddit}/about.json'
+        'https://www.reddit.com/r/{subreddit}/about.json',
+        allow_redirects=False,
     )
     if response.status_code == 200:
         return response.json()['subscribers']
