8a3d27e0d533ed8f6ff29727a298d9353f540478
Blxee <aminedev57@gmail.com>
2023-11-09 00:58:48 +0100

    Added: 0-strace_is_your_friend.pp file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # 0. Strace is your friend
 
 file_line { 'fix_spelling_mistake':
-  path  => "/var/www/html/wp-settings.php",
-  line  => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
+  path  => '/var/www/html/wp-settings.php',
+  line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
+  match => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );',
 }
