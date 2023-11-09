960ad6efcd1b3b66fbfc354798e27c437eba1d3b
Blxee <aminedev57@gmail.com>
2023-11-09 01:07:43 +0100

    Added: 0-strace_is_your_friend.pp file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # 0. Strace is your friend
 
 exec { 'fix_spelling_mistake':
-  command => 'sed -ibck "s/phpp/php/" wp-settings.php',
+  command => '/bin/sed -ibck "s/phpp/php/" wp-settings.php',
   cwd     => '/var/www/html/',
 }
