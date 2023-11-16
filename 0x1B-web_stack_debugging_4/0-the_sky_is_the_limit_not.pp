f162df04064206422ae105e0d579f25a262a523e
Blxee <aminedev57@gmail.com>
2023-11-16 20:57:05 +0100

    Added: task 0 of 0x1B-web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # 0. Sky is the limit, let's bring that limit higher
 
 exec { 'unlimit':
-  command => '/bin/sed "s/15/4096/" /etc/default/nginx',
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
   notify  => Service['nginx'],
 }
 
